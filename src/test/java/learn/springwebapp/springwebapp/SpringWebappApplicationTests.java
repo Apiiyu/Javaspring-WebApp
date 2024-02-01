2d0d13cbb3667695a9df4cdfeb821739030437d1
bts <bts@btss-MacBook-Pro.local>
2024-02-01 23:09:00 +0700

    chore: initialize commit

---
+++ src/test/java/learn/springwebapp/springwebapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.springwebapp.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
